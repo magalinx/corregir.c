e43ee9460165b9c0c348ef6f11eadead74d63bc2
magalinx <167481758+magalinx@users.noreply.github.com>
2024-04-26 17:58:47 -0300

    Add files via upload

---
+++ corregir.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+int ecu, valor=0;
+ecu=valor*valor+1;
+printf ("Si el valor=%d, el resultado es =%d\n", valor, ecu);
+valor=2;
+printf ("Si el valor=%d, el resultado es =%d\n", valor, ecu);
+return 0;
+}
+
